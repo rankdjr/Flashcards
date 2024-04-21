4063693b15bf0c1c2356b315c1e15fc057332290
rankdjr <douglas.rankjr@gmail.com>
2024-04-20 18:28:40 -0700

    added exception error print function

---
+++ Flashcards/Services/Utilities.cs
@@ -0,0 +1,19 @@
+﻿using Spectre.Console;
+
+namespace Flashcards.Services;
+
+internal class Utilities
+{
+    public static void PrintNewLines(int numOfNewLines)
+    {
+        for (int i = 0; i < numOfNewLines; i++) { Console.WriteLine(); }
+    }
+
+    public static void DisplayExceptionErrorMessage(string message, string exception)
+    {
+        PrintNewLines(1);
+        string errorMessage = $"[red]{message}[/]\n{exception}";
+        AnsiConsole.MarkupLine(errorMessage);
+        PrintNewLines(1);
+    }
+}
