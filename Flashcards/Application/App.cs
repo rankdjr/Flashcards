53adb3693b032bc0aca175473441a2c0ef1f46b9
rankdjr <douglas.rankjr@gmail.com>
2024-04-20 17:42:09 -0700

    Add project files.

---
+++ Flashcards/Application/App.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flashcards.Application;
+
+internal class App
+{
+    public App() { }
+    public void Run()
+    {
+        Console.WriteLine("Hello, World!");
+    }
+
+}
---
+++ Flashcards/Database/DatabaseContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flashcards.Database;
+
+internal class DatabaseContext
+{  
+    private readonly string _connectionString;
+    public DatabaseContext() 
+    { 
+        _connectionString = "Server=localhost;Database=FLASHCARDS;User Id=myUsername;Password=myPassword;";
+    }
+    public void Run()
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
