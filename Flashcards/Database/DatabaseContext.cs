0178f8e150170a03265e6df76758d692612f308e
rankdjr <douglas.rankjr@gmail.com>
2024-04-20 18:28:48 -0700

    setup db context class

--- Flashcards/Database/DatabaseContext.cs
+++ Flashcards/Database/DatabaseContext.cs
@@ -1,20 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Flashcards.Services;
+using System.Configuration;
+using System.Data.SqlClient;
 
 namespace Flashcards.Database;
 
 internal class DatabaseContext
-{  
+{
     private readonly string _connectionString;
-    public DatabaseContext() 
-    { 
-        _connectionString = "Server=localhost;Database=FLASHCARDS;User Id=myUsername;Password=myPassword;";
-    }
-    public void Run()
+    public DatabaseContext()
     {
-        Console.WriteLine("Hello, World!");
+        _connectionString = ConfigurationManager.ConnectionStrings["dbConnectionString"].ConnectionString;
+    }
+    public SqlConnection GetConnection()
+    {
+        var connection = new SqlConnection(_connectionString);
+        try
+        {
+            connection.Open();
+            return connection;
+            
+        }
+        catch (SqlException e)
+        {
+            Utilities.DisplayExceptionErrorMessage("Error opening database connection.", e.Message);
+            connection.Dispose();
+            throw;
+        }
     }
 }
