325844566e202cdc51dfbf5082c7284feb469329
rankdjr <douglas.rankjr@gmail.com>
2024-04-23 17:44:58 -0700

    Setup base functionality and menu

--- Flashcards/Application/App.cs
+++ Flashcards/Application/App.cs
@@ -1,5 +1,6 @@
 ﻿using Flashcards.Database;
 using Flashcards.Services;
+using Spectre.Console;
 
 namespace Flashcards.Application;
 
@@ -8,6 +9,7 @@ internal class App
     private DatabaseContext _databaseContext;
     private AppConfigurationHandler _appConfigurationHandler;
     private InputHandler _inputHandler;
+    private DatabaseSeeder _dbSeeder;
     private bool _running;
 
     public App() 
@@ -21,11 +23,68 @@ internal class App
         _appConfigurationHandler.ConfigureApplication();
 
         // Setup Services
-
+        _dbSeeder = new DatabaseSeeder(_databaseContext);
     }
 
     public void Run()
     {
-        Console.WriteLine("Hello, World!");
+        while (_running)
+        {
+            AnsiConsole.Clear();
+            DisplayMainScreenBanner();
+            PromptForSessionAction();
+        }
+    }
+
+    private void DisplayMainScreenBanner()
+    {
+        AnsiConsole.Write(
+            new FigletText("Study Hall")
+                .LeftJustified()
+                .Color(Color.SeaGreen1_1));
+
+        Utilities.PrintNewLines(2);
+    }
+
+    private void PromptForSessionAction()
+    {
+        MainMenuOption selectedOption = _inputHandler.PromptMenuSelection<MainMenuOption>();
+        ExecuteSelectedOption(selectedOption);
+    }
+
+    private void ExecuteSelectedOption(MainMenuOption option)
+    {
+        switch (option)
+        {
+            case MainMenuOption.StartStudySession:
+                //AppStopwatchManager _appStopwatchManager = new AppStopwatchManager(_codingSessionDAO, _inputHandler);
+                //_appStopwatchManager.Run();
+                break;
+            case MainMenuOption.ManageStacks:
+                //AppNewLogManager _appNewLogManager = new AppNewLogManager(_codingSessionDAO, _inputHandler);
+                //_appNewLogManager.Run();
+                break;
+            case MainMenuOption.ManageFlashCards:
+                //AppSessionManager _appSessionManager = new AppSessionManager(_codingSessionDAO, _inputHandler);
+                //_appSessionManager.Run();
+                break;
+            case MainMenuOption.ViewStudySessionData:
+                //AppGoalManager _appGoalManager = new AppGoalManager(_codingGoalDAO, _inputHandler);
+                //_appGoalManager.Run();
+                break;
+            case MainMenuOption.SeedDatabase:
+                _dbSeeder.SeedDatabase();
+                break;
+            case MainMenuOption.Exit:
+                CloseSession();
+                break;
+        }
+    }
+
+    private void CloseSession()
+    {
+        _running = false;
+        AnsiConsole.Markup("[teal]Goodbye![/]");
+        Utilities.PrintNewLines(2);
     }
 }
